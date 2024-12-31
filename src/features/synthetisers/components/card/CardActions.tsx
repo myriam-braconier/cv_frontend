d0f1dc16114ccdd2b3f2f7db2031e3b361c2da73
volubyl01 <myriam.braconier@gmail.com>
2024-12-31 17:10:13 +0100

    cardactions

--- src/features/synthetisers/components/card/CardActions.tsx
+++ src/features/synthetisers/components/card/CardActions.tsx
@@ -10,23 +10,35 @@ export const CardActions = ({
 	onEdit,
 	onDelete,
 	isAdmin,
-  }: CardActionsProps) => {
+	onDuplicate,
+	isLoading,
+}: CardActionsProps) => {
 	console.log("CardActions isAdmin:", isAdmin);
 	if (!isAdmin) return null;
-	
+
 	return (
-	  <div className="flex bg-white gap-2 justify-center items-center mt-4 p-4">
-		<button
-		  onClick={onEdit}
-		  className="max-w-[200px] bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition-colors"
-		>
-		  Éditer
-		</button>
-		<button
-		  onClick={onDelete}
-		  className="max-w-[200px] bg-red-600 text-white px-4 py-2 rounded hover:bg-red-700 transition-colors"		>
-		  Supprimer
-		</button>
-	  </div>
+		<div className="flex bg-white gap-2 justify-center items-center mt-4 p-4">
+			<button
+				onClick={onEdit}
+				disabled={isLoading}
+				className="max-w-[200px] bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition-colors"
+			>
+				Éditer
+			</button>
+			<button
+				onClick={onDelete}
+				disabled={isLoading}
+				className="max-w-[200px] bg-red-600 text-white px-4 py-2 rounded hover:bg-red-700 transition-colors"
+			>
+				Supprimer
+			</button>
+			<button
+				onClick={onDuplicate}
+				disabled={isLoading}
+				className="max-w-[200px] bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600 transition-colors"
+			>
+				Dupliquer
+			</button>
+		</div>
 	);
-  };
+};
