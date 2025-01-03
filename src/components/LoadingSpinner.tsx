990ed7866aba53b5d102c0c785d5ba1304a8629a
volubyl01 <myriam.braconier@gmail.com>
2025-01-03 12:47:09 +0100

    loadingspinner

--- src/components/LoadingSpinner.tsx
+++ src/components/LoadingSpinner.tsx
@@ -1,10 +1,16 @@
+import Image from 'next/image';
 export function LoadingSpinner() {
     return (
       <div className="col-span-full text-center p-4" role="status">
-        <div className="animate-pulse flex items-center justify-center">
-          <div className="h-8 w-8 bg-blue-500 rounded-full animate-bounce"></div>
-          <span className="ml-3">Chargement...</span>
-        </div>
-      </div>
+    <div className="flex items-center justify-center">
+      <Image 
+        src="/images/sound.gif"
+        alt="Chargement"
+        width={128}
+        height={128}
+      />
+      <span className="ml-3">Chargement...</span>
+    </div>
+  </div>
     );
   }
