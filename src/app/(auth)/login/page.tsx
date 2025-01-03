b606b769ba4c4adbe5cc0e9774e9072a6dbf4011
volubyl01 <myriam.braconier@gmail.com>
2025-01-03 21:58:32 +0100

    login page

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -6,7 +6,7 @@ export default function LoginPage() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8 " id="loginpage">
       <div className="max-w-md w-full space-y-8">
-        <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+        <h2 className="mt-6 text-center text-3xl font-extrabold text-white">
           Connexion
         </h2>
         <Suspense fallback={<div>Loading...</div>}>
