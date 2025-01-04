8ca424c2a09e2289fbb540418cb42f354a51650c
volubyl01 <myriam.braconier@gmail.com>
2025-01-04 19:21:47 +0100

    next.config.js

--- next.config.js
+++ next.config.js
@@ -9,6 +9,10 @@ module.exports = {
 				source: "/api/:path*",
 				destination: "http://localhost:4000/api/:path*",
 			},
+			{
+				source: "/about",
+				destination: "http://localhost:4000/about*",
+			},
 		];
 	},
 
